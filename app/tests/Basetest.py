8338221cee0136b2a08508f4a04671990cf96f26
judeinno <judeinno@gmail.com>
2018-08-28 16:28:51 +0300

    Add test basetest file to be handling all tests [#160083055]

---
+++ app/tests/Basetest.py
@@ -0,0 +1,46 @@
+import unittest
+from app import create_app
+from app.models.User_DBmanager import DBManager
+import json
+from instance.config import TestingConfig
+from flask import current_app as app
+
+class BaseTest (unittest.TestCase):
+
+    def createing_app(self):
+        app = create_app()
+        app.config.from_object(TestingConfig)
+        return app
+
+    def setUp(self):
+        self.app = self.createing_app()
+        self.client = self.app.test_client
+        self.app.app_context().push()
+        db = DBManager(app.config['DATABASE_URL'])
+        db.create_tables()
+
+        self.user_reg = {
+            "username": "ude",
+            "email": "ude@sample.com",
+            "password": "password"
+        }
+        self.loginlist = {
+            'username': 'jude',
+            'password': 'password'
+        }
+
+    def create_user(self):
+        response = self.client().post( '/api/v1/auth/signup',
+                                       data=json.dumps( self.user_reg ),
+                                       content_type='application/json' )
+        return response
+
+    def login_user(self):
+        response = self.client().post( '/api/v1/auth/login',
+                                       data=json.dumps( self.loginlist ),
+                                       content_type='application/json' )
+        return response
+
+    def tearDown(self):
+        db = DBManager(app.config['DATABASE_URL'])
+        db.trancate_table("users")
