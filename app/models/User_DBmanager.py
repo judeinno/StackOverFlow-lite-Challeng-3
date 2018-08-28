0fcdd6d1139914c080b8f0b941f43c8dbe3bde07
judeinno <judeinno@gmail.com>
2018-08-28 16:16:13 +0300

    Modify DB manager to update and add helper functions for endpoints [#160046782][#160046778]

--- app/models/User_DBmanager.py
+++ app/models/User_DBmanager.py
@@ -3,6 +3,7 @@ from urllib.parse import urlparse
 import psycopg2
 
 
+
 class DBManager:
     def __init__(self, database_url):
         """Initializes the connection url"""
@@ -33,10 +34,13 @@ class DBManager:
 
                """CREATE TABLE IF NOT EXISTS questions(
                 qnId SERIAL PRIMARY KEY,
+                userId VARCHAR NOT NULL,
                 Question varchar NOT NULL,
                 FOREIGN KEY (qnId) REFERENCES users(userId) ON DELETE CASCADE ON UPDATE CASCADE)""",
                """CREATE TABLE IF NOT EXISTS answers(
                          ansId SERIAL PRIMARY KEY,
+                         userId VARCHAR NOT NULL,
+                         qnId varchar NOT NULL,
                          Answers varchar NOT NULL,
                          FOREIGN KEY (ansId) REFERENCES questions(qnId) ON DELETE CASCADE ON UPDATE CASCADE)""")
         for sql_command in sql_commands:
@@ -58,9 +62,9 @@ class DBManager:
 
     def create_answer(self, data):
         """Methods to manage Question"""
-        self.cur.execute("INSERT INTO questions (Question)"
+        self.cur.execute("INSERT INTO answers (Answer)"
                          "VALUES ('{}');".format
-                         (data['Question']))
+                         (data['Answer']))
 
     def user_name_screening(self, username):
         query = "SELECT * FROM users WHERE username=%s"
@@ -80,25 +84,74 @@ class DBManager:
         Question = self.cur.fetchone()
         return Question
 
-    def view_user(self):
-        query = "SELECT email, username FROM users;"
-        self.cur.execute(query)
+    def answer_screening(self, Answer):
+        query = "SELECT * FROM answers WHERE Answer=%s"
+        self.cur.execute(query, (Answer,))
+        Question = self.cur.fetchone()
+        return Answer
+
+    def fetch_by_param(self, table_name, column, param):
+        """Fetches a single a parameter from a specific table and column"""
+        query = "SELECT * FROM {} WHERE {} = '{}'".format(
+            table_name, column, param )
+        self.cur.execute( query )
+        row = self.cur.fetchone()
+        return row
+
+
+    def fetch_by_id(self, qnId):
+        """ Gets a question by id from the questions table"""
+        self.cur.execute(
+            "SELECT * FROM questions WHERE qnId = '{}'".format(qnId))
         rows = self.cur.fetchall()
-        return rows
+        questions = []
+        for row in rows:
+            questions.append({'qnId': row[1], 'Question': row[2]})
+        return questions
+
+    def get_question(self, qnId):
+        query = "SELECT * FROM questions WHERE qnId=%s"
+        self.cur.execute(query, (qnId,))
+        qn = self.cur.fetchone()
+        if not qn:
+            return {'message': 'Question does not exist'}
+        qn_dict = {'qnId': qn[1]}
+        return qn_dict
 
     def auth_user(self, username):
         query = "SELECT * FROM users WHERE username=%s"
         self.cur.execute(query, (username,))
         user = self.cur.fetchone()
+        if not user:
+            return {'message': 'User does not exist'}
         user_dict = {'username': user[2], "password": user[3]}
         return user_dict
 
     def trancate_table(self, table):
         """Trancates the table"""
-        self.cur.execute("TRUNCATE TABLE {} RESTART IDENTITY".format(table))
+        self.cur.execute("TRUNCATE TABLE users RESTART IDENTITY CASCADE ;")
 
     def view_questions(self):
         query = "SELECT qnId, Question FROM questions;"
         self.cur.execute( query )
         rows = self.cur.fetchall()
-        return rows
+        Questions = []
+        for row in rows:
+            row = {'qnId': row[0], 'Question': row[1]}
+            Questions.append(row)
+        return Questions
+
+        query1 = "SELECT * FROM answers WHERE "
+
+    def view_question_single_id(self, qnId):
+        query = "SELECT qnId, Question FROM questions;"
+        self.cur.execute( query )
+        rows = self.cur.fetchall()
+        row_value = []
+        for row in rows:
+            if row[0] == qnId:
+                row_value.append(row)
+        return row_value
+
+
+
