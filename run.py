09c7c1db4930cff41c758b0c680fa9526ed4c49d
judeinno <judeinno@gmail.com>
2018-08-28 05:21:51 +0300

    Modify run file to add database url settings[#159947260]

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 from app import create_app
-from instance.config import DevelopmentConfig
+from instance.config import app_config
 from app.models.User_DBmanager import DBManager
 
 # The initialisation allows me to create the app in my run file where i will be running my app
-app = create_app(DevelopmentConfig)
+app = create_app('development')
 
 if __name__ == '__main__':
-    manager = DBManager()
+    manager = DBManager(app.config['DATABASE_URL'])
     manager.create_tables()
     app.run()
