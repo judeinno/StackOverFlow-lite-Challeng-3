bc6757c9f149b03c9c6e1f019551fa656d57d197
judeinno <judeinno@gmail.com>
2018-08-28 16:27:43 +0300

    Modify run file to aquire right url

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 from app import create_app
-from instance.config import app_config
 from app.models.User_DBmanager import DBManager
 
 # The initialisation allows me to create the app in my run file where i will be running my app
-app = create_app('development')
+app = create_app()
 
 if __name__ == '__main__':
     manager = DBManager(app.config['DATABASE_URL'])
