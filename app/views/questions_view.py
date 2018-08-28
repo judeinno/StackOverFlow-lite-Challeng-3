b9aa3e4a088334fd9cea244f9bc57ea98d479590
judeinno <judeinno@gmail.com>
2018-08-28 05:31:29 +0300

    Modify question views and add view to post a question [#160046782]

--- app/views/questions_view.py
+++ app/views/questions_view.py
@@ -1,7 +1,8 @@
 from app.models.User_DBmanager import DBManager
 from flask_restful import Resource
-from flask import request
+from flask import request, current_app as app
 from flask_jwt_extended import jwt_required
+
 
 
 class QuestionManager(Resource):
@@ -15,17 +16,10 @@ class QuestionManager(Resource):
         if not isinstance(Question, str ) or Question.isspace() or Question == "":
             return {"message": 'Invalid, Please enter a valid question'}, 406
 
-        db_obj = DBManager()
+        db_obj = DBManager(app.config['DATABASE_URL'])
         if db_obj.question_screening(Question):
             return {'message': 'Question already exists, please ask another question'}, 409
         else:
             db_obj.create_question(data)
             return {'message': 'You have successfully asked a question'}, 201
 
-    # @jwt_required
-    def get(self):
-        db_obj = DBManager()
-        reply = db_obj.view_questions()
-        if len( reply ) <= 0:
-            return {'message': 'no entry found'}, 400
-        return reply, 200
