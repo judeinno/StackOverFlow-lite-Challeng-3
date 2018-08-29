c2f48c146cf875a32638e68a07a133fa98d5ae0b
judeinno <judeinno@gmail.com>
2018-08-29 17:06:48 +0300

    Modify questions view file to add delete endpoint [#159947288]

--- app/views/questions_view.py
+++ app/views/questions_view.py
@@ -1,25 +1,29 @@
 from app.models.User_DBmanager import DBManager
 from flask_restful import Resource
 from flask import request, current_app as app
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
 class QuestionManager(Resource):
 
 
-    # @jwt_required
+    @jwt_required
     def post(self):
         data = request.get_json()
         Question = data['Question']
+        userid_current = get_jwt_identity()
+        current_user = userid_current['userId']
 
         if not isinstance(Question, str ) or Question.isspace() or Question == "":
             return {"message": 'Invalid, Please enter a valid question'}, 406
 
         db_obj = DBManager(app.config['DATABASE_URL'])
+        my_user_check = db_obj.fetch_by_specific_param( 'userId', 'users', 'userId', current_user )
         if db_obj.question_screening(Question):
             return {'message': 'Question already exists, please ask another question'}, 409
-        else:
-            db_obj.create_question(data)
+
+        if my_user_check:
+            db_obj.create_question(current_user, data)
             return {'message': 'You have successfully asked a question'}, 201
 
     # @jwt_required
@@ -33,7 +37,23 @@ class QuestionManager(Resource):
 
 class SingleQuestionManager(Resource):
 
+    @jwt_required
     def get(self, qnId):
         db_obj = DBManager(app.config['DATABASE_URL'])
         reply = db_obj.view_question_single_id(qnId)
         return reply
+
+
+class DeleteQuestionManager(Resource):
+
+    @jwt_required
+    def delete(self, qnId):
+        db_obj = DBManager( app.config['DATABASE_URL'] )
+        userid_current = get_jwt_identity()
+        current_user = userid_current['userId']
+        my_user_check = db_obj.fetch_by_specific_param('userId', 'users', 'userId', current_user)
+        if my_user_check:
+            if db_obj.delete_question( qnId ):
+                return 202
+            return {'message': 'Question deleted successfully'}, 202
+        return {'message': 'User does not exist'}, 400
