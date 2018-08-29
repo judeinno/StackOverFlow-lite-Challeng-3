9000315d61b7c4272cbcdc2c7e89d105e97f2585
judeinno <judeinno@gmail.com>
2018-08-29 20:05:46 +0300

    Modify api file to add endpoint url for modifies an answer [#159947368]

--- app/views/apis.py
+++ app/views/apis.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api
 from app.views.questions_view import QuestionManager, SingleQuestionManager, DeleteQuestionManager
-from app.views.answer_view import AnswerManager
+from app.views.answer_view import AnswerManager, UpdateAnswer
 
 # Initialistion of blueprint, giving it a name and a url prefix
 blue_print = Blueprint('Qns_Bp', __name__, url_prefix='/api/v1')
@@ -14,5 +14,7 @@ api.add_resource(QuestionManager, '/questions')
 api.add_resource(SingleQuestionManager, '/questions/<int:qnId>')
 
 api.add_resource(DeleteQuestionManager, '/questions/<int:qnId>')
-#
+
 api.add_resource(AnswerManager, '/questions/<int:qnId>/answers')
+
+api.add_resource(UpdateAnswer, '/questions/<int:qnId>/answers/<int:ansId>')
