216693879b1a381d27f1e10fea1f13b54d9810c5
judeinno <judeinno@gmail.com>
2018-08-30 15:54:19 +0300

    Modify questions test files to remove test data repetition [#160083055]

--- app/tests/questions_tests.py
+++ app/tests/questions_tests.py
@@ -8,16 +8,10 @@ class TestClass(BaseTest):
 
     def test_add_new_question(self):
         with self.app.test_client() as client:
-            self.client().post( '/api/v1/auth/signup',
-                                content_type='application/json',
-                                data=json.dumps( self.user_reg ) )
-            login_response = client.post( '/api/v1/auth/login', content_type='application/json',
-                                          data=json.dumps(self.user_reg) )
-            login_result = json.loads( login_response.data.decode() )
-            self.assertEqual( login_response.status_code, 200 )
-
+            self.create_user()
+            self.login_user()
             response = client.post( '/api/v1/questions', headers={'Content-Type': 'application/json',
-                                                              'Authorization': 'Bearer ' + login_result[
+                                                              'Authorization': 'Bearer ' + self.login_user()[
                                                                   'access_token']},
                                     data=json.dumps( dict( Question="What is html" ) ) )
 
@@ -27,20 +21,14 @@ class TestClass(BaseTest):
 
     def test_question_exists(self):
         with self.app.test_client() as client:
-            self.client().post( '/api/v1/auth/signup',
-                                content_type='application/json',
-                                data=json.dumps( self.user_reg ) )
-            login_response = client.post( '/api/v1/auth/login', content_type='application/json',
-                                          data=json.dumps(self.user_reg) )
-            login_result = json.loads( login_response.data.decode() )
-            self.assertEqual( login_response.status_code, 200 )
-
+            self.create_user()
+            self.login_user()
             client.post( '/api/v1/questions', headers={'Content-Type': 'application/json',
-                                                                  'Authorization': 'Bearer ' + login_result[
+                                                                  'Authorization': 'Bearer ' + self.login_user()[
                                                                       'access_token']},
                                     data=json.dumps( dict( Question="What is html" ) ) )
             response = client.post( '/api/v1/questions', headers={'Content-Type': 'application/json',
-                                                              'Authorization': 'Bearer ' + login_result[
+                                                              'Authorization': 'Bearer ' + self.login_user()[
                                                                   'access_token']},
                                     data=json.dumps( dict( Question="What is html" ) ) )
 
@@ -50,16 +38,10 @@ class TestClass(BaseTest):
 
     def test_question_invalid(self):
         with self.app.test_client() as client:
-            self.client().post( '/api/v1/auth/signup',
-                                content_type='application/json',
-                                data=json.dumps( self.user_reg ) )
-            login_response = client.post( '/api/v1/auth/login', content_type='application/json',
-                                          data=json.dumps(self.user_reg) )
-            login_result = json.loads( login_response.data.decode() )
-            self.assertEqual( login_response.status_code, 200 )
-
+            self.create_user()
+            self.login_user()
             response = client.post( '/api/v1/questions', headers={'Content-Type': 'application/json',
-                                                              'Authorization': 'Bearer ' + login_result[
+                                                              'Authorization': 'Bearer ' + self.login_user()[
                                                                   'access_token']},
                                     data=json.dumps( dict( Question="" ) ) )
 
@@ -69,16 +51,10 @@ class TestClass(BaseTest):
 
     def test_question_get(self):
         with self.app.test_client() as client:
-            self.client().post( '/api/v1/auth/signup',
-                                content_type='application/json',
-                                data=json.dumps( self.user_reg ) )
-            login_response = client.post( '/api/v1/auth/login', content_type='application/json',
-                                          data=json.dumps(self.user_reg) )
-            login_result = json.loads( login_response.data.decode() )
-            self.assertEqual( login_response.status_code, 200 )
-
+            self.create_user()
+            self.login_user()
             client.post( '/api/v1/questions', headers={'Content-Type': 'application/json',
-                                                              'Authorization': 'Bearer ' + login_result[
+                                                              'Authorization': 'Bearer ' + self.login_user()[
                                                                   'access_token']},
                                     data=json.dumps( dict( Question="What is html" ) ) )
             response = client.get( '/api/v1/questions', content_type='application/json' )
@@ -88,12 +64,8 @@ class TestClass(BaseTest):
 
     def test_get_question_that_doesnt_exist(self):
         with self.app.test_client() as client:
-            self.client().post( '/api/v1/auth/signup',
-                                content_type='application/json',
-                                data=json.dumps( self.user_reg ) )
-            login_response = client.post( '/api/v1/auth/login', content_type='application/json',
-                                          data=json.dumps(self.user_reg) )
-            self.assertEqual( login_response.status_code, 200 )
+            self.create_user()
+            self.login_user()
             response = client.get( '/api/v1/questions', content_type='application/json',
                                     data=json.dumps( dict( Question="What is html" ) ) )
             responseJson = json.loads( response.data.decode() )
