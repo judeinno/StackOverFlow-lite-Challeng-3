baf203145ef66599eb4d12be35697bc15db0beb5
judeinno <judeinno@gmail.com>
2018-08-31 09:59:55 +0300

    Add answers basetest file to test answers [#160083055]

---
+++ app/tests/basetest_answers.py
@@ -0,0 +1,58 @@
+import unittest
+from app import create_app
+from app.models.User_DBmanager import DBManager
+import json
+from instance.config import TestingAnswersConfig
+from flask import current_app as app
+
+class BaseTest (unittest.TestCase):
+
+    def creating_app(self):
+        app = create_app()
+        app.config.from_object(TestingAnswersConfig)
+        return app
+
+    def setUp(self):
+        self.app = self.creating_app()
+        self.client = self.app.test_client
+        self.app.app_context().push()
+        db = DBManager(app.config['DATABASE_URL'])
+        db.create_tables()
+
+        self.user_reg = {
+            "username": "jude",
+            "email": "jude@sample.com",
+            "password": "password"
+        }
+
+        self.loginlist = {
+            'username': 'jude',
+            'password': 'password'
+        }
+        self.user_reg_sec = {
+            "username": "irene",
+            "email": "irene@sample.com",
+            "password": "password"
+        }
+
+        self.loginlist_sec = {
+            'username': 'irene',
+            'password': 'password'
+        }
+
+    def create_user(self):
+        response = self.client().post( '/api/v1/auth/signup',
+                                       data=json.dumps( self.user_reg ),
+                                       content_type='application/json' )
+        return response
+
+    def login_user(self):
+        login_response = self.client().post( '/api/v1/auth/login',
+                                       data=json.dumps( self.loginlist ),
+                                       content_type='application/json' )
+        login_result = json.loads( login_response.data.decode() )
+        return login_result
+
+    def tearDown(self):
+        db = DBManager(app.config['DATABASE_URL'])
+        db.drop_table()
